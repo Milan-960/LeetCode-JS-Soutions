a6180c79161670880ef8fc219314528a8a29144a
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-02-27 13:12:51 +0100

    reverse words algo has been resolved

---
+++ LeetCode-JS/Algorithm_1_challange/Day4_Two Pointers/557. Reverse Words in a String III/557. Reverse Words in a String III.js
@@ -0,0 +1,33 @@
+// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+// Example 1:
+
+// Input: s = "Let's take LeetCode contest"
+// Output: "s'teL ekat edoCteeL tsetnoc"
+// Example 2:
+
+// Input: s = "God Ding"
+// Output: "doG gniD"
+
+// Constraints:
+
+// 1 <= s.length <= 5 * 104
+// s contains printable ASCII characters.
+// s does not contain any leading or trailing spaces.
+// There is at least one word in s.
+// All the words in s are separated by a single space.
+
+// Ans
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  const words = s.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    words[i] = word.split("").reverse().join("");
+  }
+  return words.join(" ");
+};
