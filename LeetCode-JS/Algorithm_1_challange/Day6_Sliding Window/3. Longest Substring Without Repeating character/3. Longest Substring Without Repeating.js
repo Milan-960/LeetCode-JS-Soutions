fb7ebba3534a4dd4773e41124e8fd0e4ffd8ec14
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-03-01 15:03:04 +0100

    was alreaedy solved

---
+++ LeetCode-JS/Algorithm_1_challange/Day6_Sliding Window/3. Longest Substring Without Repeating character/3. Longest Substring Without Repeating.js
@@ -0,0 +1,61 @@
+// Given a string s, find the length of the longest
+// substring
+//  without repeating characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+// ANS
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  const ss = new Set();
+  let i = 0;
+  let ans = 0;
+  for (let j = 0; j < s.length; ++j) {
+    while (ss.has(s[j])) {
+      ss.delete(s[i++]);
+    }
+    ss.add(s[j]);
+    ans = Math.max(ans, j - i + 1);
+  }
+  return ans;
+};
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  const ss = new Set();
+  let i = 0;
+  let ans = 0;
+  for (let j = 0; j < s.length; ++j) {
+    while (ss.has(s[j])) {
+      ss.delete(s[i++]);
+    }
+    ss.add(s[j]);
+    ans = Math.max(ans, j - i + 1);
+  }
+  return ans;
+};
